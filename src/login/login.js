fa8afca3c36be071294b5cd94816527096f0f806
Stella <yeobinlee1210@gmail.com>
2026-06-24 22:18:25 +0900

    feat(signup): API  연결 확인 기능 임시 구현

--- src/login/login.js
+++ src/login/login.js
@@ -1,2 +1,31 @@
 const loginForm = document.getElementById('login-form');
 const loginBtn = document.getElementById('login-btn');
+
+loginBtn.addEventListener('click', async (event) => {
+    /*
+    try{
+        const response = await fetch('http://localhost:8080/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: "test@test.com",
+                password: "test12345"
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error('로그인 실패');
+        }
+
+        const data = await response.json();
+        console.log('로그인 성공:', data);
+        window.location.replace('../board/board.html');
+
+    }catch(error){
+        console.error('로그인 중 오류 발생:', error);
+    }
+*/
+    window.location.replace('../board/board.html');
+});
---
+++ src/signup/signup.js
@@ -0,0 +1,35 @@
+const signupBtn = document.getElementById('signup-btn');
+
+const profilePictureInput = "temp_image.jpg";
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const nicknameInput = document.getElementById('nickname');
+
+signupBtn.addEventListener('click', async (event) => {
+    /*
+try{
+        const response = await fetch('http://localhost:8080/users/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: emailInput.value,
+                password: passwordInput.value,
+                nickname: nicknameInput.value,
+                profilePicture: profilePictureInput
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error('회원가입 실패');
+        }
+
+        const data = await response.json();
+        console.log('회원가입 성공:', data);
+    }catch(error){
+        console.error('회원가입 중 오류 발생:', error);
+    }
+        */
+    window.location.replace('../login/login.html');
+});
