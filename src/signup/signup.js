6590a1d34bebf28ec36e3831a5147de57cdc5739
Stella <yeobinlee1210@gmail.com>
2026-06-30 12:56:18 +0900

    feat(signup): 회원가입 검증기능 추가

--- src/signup/signup.js
+++ src/signup/signup.js
@@ -1,18 +1,74 @@
 import { loadHeader } from "../components/header/header.js";
+import { pfpHelperTextMaker, emailHelperTextMaker, pwdHelperTextMaker, confirmPwdHelperTextMaker, nicknameHelperTextMaker} from "../utils/helperTextMaker.js";
 
 loadHeader();
 
-const signupBtn = document.getElementById('signup-btn');
 
 const profilePictureInput = document.getElementById('profilePicture');
-const emailInput = document.getElementById('email');
-const passwordInput = document.getElementById('password');
-const nicknameInput = document.getElementById('nickname');
+const pfpHelperText = document.getElementById('pfpHelperText');
 
+const emailInput = document.getElementById('email');
+const emailHelperText =document.getElementById('emailHelperText');
+
+const passwordInput = document.getElementById('password');
+const pwdHelperText = document.getElementById('pwdHelperText');
+
+const confirmPwdInput = document.getElementById('confirmPassword');
+const confirmPwdHelperText = document.getElementById('confirmPwdHelperText');
+
+const nicknameInput = document.getElementById('nickname');
+const nicknameHelperText = document.getElementById('nicknameHelperText');
+
+const signupBtn = document.getElementById('signupBtn');
+
+const checkAllInputFilled = () =>{
+    if(profilePictureInput.value.length !== 0 
+        && emailInput.value.length !== 0
+        && passwordInput.value.length !== 0
+        && confirmPwdInput.value.length !== 0
+        && nicknameInput.value.lenhth !== 0) {
+          signupBtn.style.backgroundColor = "#7f6aee"
+        }
+    signupBtn.style.backgroundColor = "#aca0eb"
+}
+
+profilePictureInput.addEventListener('change', (event) =>{
+    let helperTextMsg;
+    if(event.target.files[0]?.name){
+        helperTextMsg = pfpHelperTextMaker(event.target.files[0].name);
+    }else{
+        helperTextMsg = pfpHelperTextMaker(null)
+    }
+    pfpHelperText.textContent = helperTextMsg;
+})
+
+emailInput.addEventListener('input', ()=>{
+    const helperTextMsg = emailHelperTextMaker(emailInput.value);
+    emailHelperText.textContent = helperTextMsg;
+    checkAllInputFilled()
+})
+
+passwordInput.addEventListener('input', () =>{
+    const helperTextMsg = pwdHelperTextMaker(passwordInput.value, confirmPwdInput.value);
+    pwdHelperText.textContent = helperTextMsg;
+    checkAllInputFilled()
+})
+
+confirmPwdInput.addEventListener('input', () => {
+    const helperTextMsg = confirmPwdHelperTextMaker(passwordInput.value, confirmPwdInput.value);
+    confirmPwdHelperText.textContent = helperTextMsg;
+    checkAllInputFilled()
+})
+
+nicknameInput.addEventListener('input', ()=>{
+    const helperTextMsg = nicknameHelperTextMaker(nickname.value);
+    nicknameHelperText.textContent = helperTextMsg;
+    checkAllInputFilled()
+})
 
 signupBtn.addEventListener('click', async (event) => {
-    
-try{
+
+    try{
         const response = await fetch('http://localhost:8080/users/signup', {
             method: 'POST',
             headers: {
