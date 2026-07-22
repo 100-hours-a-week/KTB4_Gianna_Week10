bfc292090cbf9985da85144a008e2b43a7a9a937
Stella <yeobinlee1210@gmail.com>
2026-07-22 17:43:23 +0900

    chore: class를 className으로 수정

--- src/LoginPage/components/LoginForm.jsx
+++ src/LoginPage/components/LoginForm.jsx
@@ -2,25 +2,25 @@ export const LoginForm = ({onEmailChange, onPasswordChange, emailHelperText, pwd
 
     return(
         <form id="login-form" action="http://localhost:8080/login-process" method="post">
-            <label for="email">이메일</label>
+            <label htmlFor="email">이메일</label>
             <input 
                 id="email" 
-                class ="email-label" 
+                className ="email-label" 
                 name="email" 
                 type="text" 
                 placeholder="이메일을 입력하세요" 
                 onChange={(event)=>{ onEmailChange(event.target.value) }}/>
-            <p id="emailHelperText" class="helper-text">{emailHelperText}</p>
+            <p id="emailHelperText" className="helper-text">{emailHelperText}</p>
 
-            <label for="password">비밀번호</label>
+            <label htmlFor="password">비밀번호</label>
             <input 
             id="password" 
-            class="pwd-label" 
+            className="pwd-label" 
             name="password" 
             type="password" 
             placeholder="비밀번호를 입력하세요"
             onChange={(event)=>{onPasswordChange(event.target.value)}}/>
-            <p id="pwdHelperText" class="helper-text">{pwdHelperText}</p>
+            <p id="pwdHelperText" className="helper-text">{pwdHelperText}</p>
             <button id="loginBtn" type="submit" disabled = {!isLoginEnabled}>로그인</button>
         </form>
     );
