ee91d22716d3cee86e6929fa79977c9f464cb501
Stella <yeobinlee1210@gmail.com>
2026-07-22 21:58:23 +0900

    refactor(BoardPage): 게시물 목록 구현

--- src/board/BoardPage.jsx
+++ src/board/BoardPage.jsx
@@ -1,20 +1,57 @@
 import './BoardPage.css'
 import { useNavigate } from 'react-router-dom'
+import { useState, useEffect} from 'react';
+import { requestCsrfAPIJsonResponse } from '../api/csrf';
+import { PostItem } from './components/PostItem';
+
 export const BoardPage = () =>{
     const navigate = useNavigate();
+    const [postItems, setPostItems] = useState([]);
     
-    async function getPostList(){
+    useEffect(()=>{
+        async function getPostList(){
+        const csrf = await requestCsrfAPIJsonResponse();
+        try{
+        const response = await fetch('http://localhost:8080/posts', {
+            method: 'GET',
+            credentials:"include",
+            headers: {
+                [csrf.headerName] : csrf.token    
+            }      
+        });
+
+        if (!response.ok) {
+            throw new Error('게시물 목록 조회 실패');
+        }
+
+        const data = await response.json();
+        const postList = data?.data?.postList || [];
+        console.log(postList)
+        setPostItems(postList)
+    } catch(error){
+        console.error('오류 발생:', error);
     }
+}
+    getPostList();
+    },[])
+
     return(
         <>
             <div id="headerContainer"></div>
-            <main class="board-page">
-                <section class="board-intro">
+            <main className="board-page">
+                <section className="board-intro">
                     <h1>온전한 '나의 취향'을 <br/>만들어가는 공간. <br/></h1>
-                    <button class="write-post-link" type="button" onClick={()=>{navigate('/posts/write')}}>게시글 작성</button>
+                    <button className="write-post-link" type="button" onClick={()=>{navigate('/posts/write')}}>게시글 작성</button>
                 </section>
                 
-                <section id="post-list-container" class="post-list-container">{}</section>
+                <section id="post-list-container" className="post-list-container">
+                    {postItems.map((post, idx)=>{
+                        return <PostItem
+                        key={post.id}
+                        post={post}
+                        index={idx}
+                        />
+                })}</section>
             </main> 
         </>
     )
